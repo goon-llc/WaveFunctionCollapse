55b0864b4e997b1eac121e2b04caffae7377e16e
Will Holland <will.holland@go-on-entertainment.com>
2026-02-06 20:35:50 -0500

    more variable renames for consistency

--- InvocationExample.cs
+++ InvocationExample.cs
@@ -17,49 +17,49 @@ static class InvocationExample
     Random random = new( );
     XDocument xdoc = XDocument.Load( "samples.xml" );
 
-    foreach ( XElement xelem in xdoc.Root.Elements( "overlapping", "simpletiled" ) )
+    foreach ( XElement xElement in xdoc.Root.Elements( "overlapping", "simpletiled" ) )
     {
       Model model;
-      string name = xelem.Get<string>( "name" );
+      string name = xElement.Get<string>( "name" );
       Console.WriteLine( $"< {name}" );
 
-      bool isOverlapping = xelem.Name == "overlapping";
-      int size = xelem.Get( "size", isOverlapping ? 48 : 24 );
-      int width = xelem.Get( "width", size );
-      int height = xelem.Get( "height", size );
-      bool periodic = xelem.Get( "periodic", false );
-      string heuristicString = xelem.Get<string>( "heuristic" );
+      bool isOverlapping = xElement.Name == "overlapping";
+      int size = xElement.Get( "size", isOverlapping ? 48 : 24 );
+      int width = xElement.Get( "width", size );
+      int height = xElement.Get( "height", size );
+      bool periodic = xElement.Get( "periodic", false );
+      string heuristicString = xElement.Get<string>( "heuristic" );
       var heuristic = heuristicString == "Scanline" ? Model.Heuristic.Scanline : ( heuristicString == "MRV" ? Model.Heuristic.MRV : Model.Heuristic.Entropy );
 
       if ( isOverlapping )
       {
-        int n = xelem.Get( "N", 3 );
-        bool periodicInput = xelem.Get( "periodicInput", true );
-        int symmetry = xelem.Get( "symmetry", 8 );
-        bool ground = xelem.Get( "ground", false );
+        int n = xElement.Get( "N", 3 );
+        bool periodicInput = xElement.Get( "periodicInput", true );
+        int symmetry = xElement.Get( "symmetry", 8 );
+        bool ground = xElement.Get( "ground", false );
 
         model = new OverlappingModel( name, n, width, height, periodicInput, periodic, symmetry, ground, heuristic );
       }
       else
       {
-        string subset = xelem.Get<string>( "subset" );
-        bool blackBackground = xelem.Get( "blackBackground", false );
+        string subset = xElement.Get<string>( "subset" );
+        bool blackBackground = xElement.Get( "blackBackground", false );
 
         model = new SimpleTiledModel( name, subset, width, height, periodic, blackBackground, heuristic );
       }
 
-      for ( int i = 0; i < xelem.Get( "screenshots", 2 ); i++ )
+      for ( int i = 0; i < xElement.Get( "screenshots", 2 ); i++ )
       {
         for ( int k = 0; k < 10; k++ )
         {
           Console.Write( "> " );
           int seed = random.Next( );
-          bool success = model.Run( seed, xelem.Get( "limit", -1 ) );
+          bool success = model.Run( seed, xElement.Get( "limit", -1 ) );
           if ( success )
           {
             Console.WriteLine( "DONE" );
             model.Save( $"output/{name} {seed}.png" );
-            if ( model is SimpleTiledModel stmodel && xelem.Get( "textOutput", false ) )
+            if ( model is SimpleTiledModel stmodel && xElement.Get( "textOutput", false ) )
               System.IO.File.WriteAllText( $"output/{name} {seed}.txt", stmodel.TextOutput( ) );
             break;
           }
