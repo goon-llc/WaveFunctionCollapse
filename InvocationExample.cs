e0680b7847d4ed5b7a1ee12329bd35bb77af72c1
Will Holland <will.holland@go-on-entertainment.com>
2026-02-07 21:04:17 -0500

    rm simple tile model

--- InvocationExample.cs
+++ InvocationExample.cs
@@ -17,36 +17,25 @@ static class InvocationExample
     Random random = new( );
     XDocument xdoc = XDocument.Load( "samples.xml" );
 
-    foreach ( XElement xElement in xdoc.Root.Elements( "overlapping", "simpletiled" ) )
+    foreach ( XElement xElement in xdoc.Root.Elements( "overlapping" ) )
     {
       Model model;
       string name = xElement.Get<string>( "name" );
       Console.WriteLine( $"< {name}" );
 
-      bool isOverlapping = xElement.Name == "overlapping";
-      int size = xElement.Get( "size", isOverlapping ? 48 : 24 );
+      int size = xElement.Get( "size", 48 );
       int width = xElement.Get( "width", size );
       int height = xElement.Get( "height", size );
       bool periodic = xElement.Get( "periodic", false );
       string heuristicString = xElement.Get<string>( "heuristic" );
       var heuristic = heuristicString == "Scanline" ? Model.Heuristic.Scanline : ( heuristicString == "MRV" ? Model.Heuristic.MRV : Model.Heuristic.Entropy );
 
-      if ( isOverlapping )
-      {
-        int n = xElement.Get( "N", 3 );
-        bool periodicInput = xElement.Get( "periodicInput", true );
-        int symmetry = xElement.Get( "symmetry", 8 );
-        bool ground = xElement.Get( "ground", false );
+      int n = xElement.Get( "N", 3 );
+      bool periodicInput = xElement.Get( "periodicInput", true );
+      int symmetry = xElement.Get( "symmetry", 8 );
+      bool ground = xElement.Get( "ground", false );
 
-        model = new OverlappingModel( name, n, width, height, periodicInput, periodic, symmetry, ground, heuristic );
-      }
-      else
-      {
-        string subset = xElement.Get<string>( "subset" );
-        bool blackBackground = xElement.Get( "blackBackground", false );
-
-        model = new SimpleTiledModel( name, subset, width, height, periodic, blackBackground, heuristic );
-      }
+      model = new OverlappingModel( name, n, width, height, periodicInput, periodic, symmetry, ground, heuristic );
 
       for ( int i = 0; i < xElement.Get( "screenshots", 2 ); i++ )
       {
@@ -59,11 +48,9 @@ static class InvocationExample
           {
             Console.WriteLine( "DONE" );
             model.Save( $"output/{name} {seed}.png" );
-            if ( model is SimpleTiledModel stmodel && xElement.Get( "textOutput", false ) )
-              System.IO.File.WriteAllText( $"output/{name} {seed}.txt", stmodel.TextOutput( ) );
             break;
           }
-          else Console.WriteLine( "CONTRADICTION" );
+          Console.WriteLine( "CONTRADICTION" );
         }
       }
     }
