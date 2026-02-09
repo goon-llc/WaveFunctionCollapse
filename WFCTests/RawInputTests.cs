6c0f0fc3fa4810d79858b779379dce9b274c0127
whhthe2 <whhthe2@gmail.com>
2026-02-09 17:11:47 -0500

    more experimenting with model params

--- WFCTests/RawInputTests.cs
+++ WFCTests/RawInputTests.cs
@@ -1,44 +1,59 @@
 using System;
+using System.IO;
 using WFC;
 
 namespace WFCTests;
 
 public class RawInputTests
 {
-  [ Fact ]
-  public void MineLayoutTest( )
+  private DirectoryInfo MakeTestOutputFolder( string testName )
   {
-    var folder = System.IO.Directory.CreateDirectory( "output" );
+    var folder = System.IO.Directory.CreateDirectory( $"output/{testName}" );
     foreach ( var file in folder.GetFiles( ) )
     {
       file.Delete( );
     }
+    return folder;
+  }
+  
+  [ Fact ]
+  public void ArgOutOfRangeCase( )
+  {
+    var outDir = MakeTestOutputFolder(nameof(ArgOutOfRangeCase));
     
-
-    var layoutName = "mine02";
-
+    var layoutName = "mine05";
     Model model = new OverlappingModel(
       name: layoutName,
       n: 3,
-      outWidth: 48,
-      outHeight: 48,
+      outWidth: 256,
+      outHeight: 256,
       periodicInput: false,
-      periodicOutput: true,
-      symmetry: 8,
-      ground: false,
-      Model.Heuristic.Entropy
+      periodicOutput: false,
+      symmetry: 1,
+      ground: true,
+      Model.Heuristic.Scanline
     );
 
-    int seed = 42;
-    var success = model.Run( seed, -1 );
+    int tries = 0;
+    int maxTries = 10;
+    bool success = false;
+
+    while ( !success && tries < maxTries )
+    {
+      tries++;
+      success = model.Run( tries+8, -1 );
+    }
 
     if ( success )
-    {
-      model.Save( $"output/{layoutName}_{seed}.png" );
+    { 
+      model.Save( Path.Combine( outDir.FullName, $"{layoutName}_{tries}.png" ) );
+      /*Assert.Throws<ArgumentNullException>( ( ) =>
+      {
+      } );*/
     }
     else
     {
-      Console.WriteLine( $"Failed to generate coherent layout with {layoutName}.png." );
+      throw new Exception( $"Failed to generate coherent layout with {layoutName}.png after {maxTries} tries." );
     }
   }
 }
