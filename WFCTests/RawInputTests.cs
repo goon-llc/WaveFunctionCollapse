e3d17dcc996be88a99d9df4fa873a9e53bb09f02
Will Holland <will.holland@go-on-entertainment.com>
2026-02-10 10:00:19 -0500

    test helpers

--- WFCTests/RawInputTests.cs
+++ WFCTests/RawInputTests.cs
@@ -17,11 +17,68 @@ public class RawInputTests
     return folder;
   }
 
+
+  private bool Retry( Model model, int seed = 42 )
+  {
+    int tries = 0;
+    int maxTries = 15;
+    bool success = false;
+    var random = new Random( seed );
+    while ( !success && tries < maxTries )
+    {
+      tries++;
+      success = model.Run( random.Next() + 1_000_000, -1 );
+    }
+    return success;
+  }
+
+  [ Fact ]
+  private void BasicMineLayoutScanline( )
+  {
+    var folder = MakeTestOutputFolder( nameof(BasicMineLayoutScanline) );
+    var layoutName = "mine01";
+    var scanline = new OverlappingModel(
+      name: layoutName,
+      n: 3,
+      outWidth: 128,
+      outHeight: 128,
+      periodicInput: false,
+      periodicOutput: false,
+      symmetry: 1,
+      ground: false,
+      Model.Heuristic.Scanline );
+    
+    var outputPath = Path.Combine( folder.FullName, $"{layoutName}_scanline.png" );
+    if ( Retry( scanline ) ) scanline.Save( outputPath );
+    else throw new Exception( $"Failed to generate coherent layout with {layoutName}.png" );
+  }
+
+  [ Fact ]
+  private void BasicMineLayoutEntropy( )
+  {
+    var folder = MakeTestOutputFolder( nameof(BasicMineLayoutEntropy) );
+    var layoutName = "mine01";
+    var scanline = new OverlappingModel(
+      name: layoutName,
+      n: 3,
+      outWidth: 128,
+      outHeight: 128,
+      periodicInput: false,
+      periodicOutput: false,
+      symmetry: 1,
+      ground: false,
+      Model.Heuristic.Entropy );
+    
+    var outputPath = Path.Combine( folder.FullName, $"{layoutName}_entropy.png" );
+    if ( Retry( scanline ) ) scanline.Save( outputPath );
+    else throw new Exception( $"Failed to generate coherent layout with {layoutName}.png" );
+  }
+  
+
   [ Fact ]
   public void ArgOutOfRangeCase( )
   {
     var outDir = MakeTestOutputFolder( nameof(ArgOutOfRangeCase) );
-
     var layoutName = "mine02";
     Model model = new OverlappingModel(
       name: layoutName,
@@ -31,8 +88,8 @@ public class RawInputTests
       periodicInput: false,
       periodicOutput: false,
       symmetry: 1,
-      ground: true,
-      Model.Heuristic.MRV
+      ground: false,
+      Model.Heuristic.Scanline
     );
 
     int tries = 0;
