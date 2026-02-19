c0d62170f8ea58119910fa2971e0e58dc6489c54
whhthe2 <whhthe2@gmail.com>
2026-02-19 10:45:41 -0500

    add config object and alt constructor

---
+++ WFC/WFCConfig.cs
@@ -0,0 +1,6 @@
+﻿namespace WFC
+{
+  public record WFCConfig(
+    int[] brush, int inputWidth, int inputHeight, int n, int outputWidth, int outputHeight,
+    bool periodicInput, bool periodicOutput, int symmetry, bool ground, Model.Heuristic heuristic );
+}
